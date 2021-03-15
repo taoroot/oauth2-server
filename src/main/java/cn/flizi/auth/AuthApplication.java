1f2d935bf5de91c557f3dc45214a3de33aec5889
zhiyi <lujiantaoxyz@outlook.com>
2021-03-15 22:50:27 +0800

    0.0.1

--- src/main/java/cn/flizi/auth/AuthApplication.java
+++ src/main/java/cn/flizi/auth/AuthApplication.java
@@ -1,36 +1,12 @@
 package cn.flizi.auth;
 
-import cn.flizi.auth.properties.CaptchaProperties;
-import cn.flizi.auth.properties.SmsProperties;
-import cn.flizi.auth.properties.SocialProperties;
-import cn.flizi.auth.security.social.SocialAuthenticationProvider;
-import cn.flizi.auth.util.DingTalkUtil;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthApplication implements CommandLineRunner {
-    private static final Logger log = LogManager.getLogger(SocialAuthenticationProvider.class);
-
-    @Autowired
-    private CaptchaProperties captchaProperties;
-    @Autowired
-    private SocialProperties socialProperties;
-    @Autowired
-    private SmsProperties smsProperties;
+public class AuthApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        log.info(captchaProperties);
-        log.info(socialProperties);
-        log.info(smsProperties);
     }
 }
