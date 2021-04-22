bee087463879e33e8858ceb695e95724436c51ca
zhiyi <zy@flizi.cn>
2021-04-22 14:29:59 +0800

    0.0.1

---
+++ common/common-core/src/main/java/cn/flizi/cloud/core/util/R.java
@@ -0,0 +1,69 @@
+package cn.flizi.cloud.core.util;
+
+import lombok.*;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class R<T> implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    public static final String OK = "SUCCESS";
+    public static final String ERROR = "ERROR";
+
+    @Getter
+    @Setter
+    private String code;
+
+    @Getter
+    @Setter
+    private String msg;
+
+    @Getter
+    @Setter
+    private T data;
+
+    public static <T> R<T> ok() {
+        return instance(null, OK, null);
+    }
+
+    public static <T> R<T> ok(T data) {
+        return instance(data, OK, null);
+    }
+
+    public static <T> R<T> ok(T data, String msg) {
+        return instance(data, OK, msg);
+    }
+
+    public static <T> R<T> okMsg(String msg) {
+        return instance(null, OK, msg);
+    }
+
+    public static <T> R<T> error() {
+        return instance(null, ERROR, null);
+    }
+
+    public static <T> R<T> error(T data) {
+        return instance(data, ERROR, null);
+    }
+
+    public static <T> R<T> error(T data, String msg) {
+        return instance(data, ERROR, msg);
+    }
+
+    public static <T> R<T> errMsg(String msg) {
+        return instance(null, ERROR, msg);
+    }
+
+    public static <T> R<T> instance(T data, String code, String msg) {
+        R<T> apiResult = new R<>();
+        apiResult.setCode(code);
+        apiResult.setData(data);
+        apiResult.setMsg(msg);
+        return apiResult;
+    }
+}
---
+++ oauth-2.1/src/main/java/cn/flizi/cloud/auth/App.java
@@ -0,0 +1,7 @@
+package cn.flizi.cloud.auth;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
